02ca8d207b819a0282efcaf7d040b20157b2d131
Roger Santos <rogersw06@gmail.com>
2024-04-19 09:29:42 -0300

    Commit final com todos os estilos ajustado.

--- src/components/FormVagas/styles.ts
+++ src/components/FormVagas/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const Formulario = styled.form`
+export const Form = styled.form`
   display: grid;
   grid-template-columns: 1fr auto;
   background-color: var(--cor-secundaria);
@@ -9,7 +9,7 @@ export const Formulario = styled.form`
   margin-top: 40px;
 `
 
-export const Botao = styled.input`
+export const BtnPesquisar = styled.button`
   background-color: var(--cor-principal);
   border: 1px solid var(--cor-principal);
   height: 40px;
@@ -20,7 +20,7 @@ export const Botao = styled.input`
   cursor: pointer;
 `
 
-export const Campo = styled.div`
+export const Campo = styled.input`
   padding: 0 16px;
   outline-color: var(--cor-principal);
 `
