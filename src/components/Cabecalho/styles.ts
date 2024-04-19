75982b1a5fdf871c98be2bb2ff1d1fab58197a23
Roger Santos <rogersw06@gmail.com>
2024-04-19 08:55:39 -0300

    Alteração do container ListaVagas

--- src/components/Cabecalho/styles.ts
+++ src/components/Cabecalho/styles.ts
@@ -1,6 +1,10 @@
-.cabecalho {
+import styled from 'styled-components'
+
+const CabecalhoStyle = styled.header`
   background-color: var(--cor-secundaria);
   color: var(--cor-principal);
   text-align: center;
   padding: 24px 0;
-}
+`
+
+export default CabecalhoStyle
